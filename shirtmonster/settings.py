3e611bcb5608752dab7de08bade96d10d567e6ab
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-13 07:47:56 +0100

    Hide config vars

--- shirtmonster/settings.py
+++ shirtmonster/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.isfile('env.py'):
+	import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u^-l)pt1i+8mg9ch1l=%_und^if2ddymh#nap^^$d-+eg@jrj5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEBUG" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("DEV_URL"), os.environ.get("PROD_URL")]
 
 
 # Application definition
