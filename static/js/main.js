f4e6c3fdbe64a4a30cc021bce2815764f318cc17
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-15 09:45:39 +0100

    Set up carousel

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,8 @@
 /* This snippet is required to render the Lucide Icons -> https://lucide.dev/guide/packages/lucide */
 lucide.createIcons();
+
+/* Custom settings for the bootstrap carousel */
+$('.carousel').carousel({
+    interval: 5000,
+    touch: true,
+})
