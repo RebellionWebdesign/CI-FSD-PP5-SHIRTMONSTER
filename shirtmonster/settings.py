361efc975ee8aa263b1aef43df6bc97f0dc48943
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-16 08:38:42 +0100

    Install whitenoise for static files

--- shirtmonster/settings.py
+++ shirtmonster/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,7 +150,9 @@ STATIC_URL = '/static/'
 if "DEBUG" in os.environ:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 else:
-    STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT  = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
