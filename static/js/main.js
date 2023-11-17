fcba0e4cbb17e46b8a34789a15d1f0337fa74c99
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-17 09:39:54 +0100

    Add header hide function

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,29 @@
 /* This snippet is required to render the Lucide Icons -> https://lucide.dev/guide/packages/lucide */
 lucide.createIcons();
 
-/* Custom settings for the bootstrap carousel */
+/* Custom settings for the bootstrap carousel -> https://getbootstrap.com/docs/4.6/components/carousel/ */
 $('.carousel').carousel({
     interval: 5000,
     touch: true,
 })
+
+/* Checks if the url is one of the login/logout/password urls and hides the header */
+function hideHeader() {
+    let loginUrl = window.location.href.includes('login');
+    let logoutUrl = window.location.href.includes('logout');
+    let signupUrl = window.location.href.includes('signup');
+    let resetUrl = window.location.href.includes('reset');
+    const header = document.getElementById('header')
+
+    if (loginUrl) {
+        header.style.display("none")
+    } else if (logoutUrl) {
+        header.style.display("none")
+    } else if (signupUrl) {
+        header.style.display("none")
+    } else if (resetUrl) {
+        header.style.display("none")
+    }
+}
+
+hideHeader();
