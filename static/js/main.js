f9e2fbb2a1e55c2df590ab4f48d8aa84d2f47e79
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-18 13:50:39 +0100

    Add mobile menu and mobile search script

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,8 @@
-/* This snippet is required to render the Lucide Icons -> https://lucide.dev/guide/packages/lucide */
-lucide.createIcons();
-
-/* Custom settings for the bootstrap carousel -> https://getbootstrap.com/docs/4.6/components/carousel/ */
-$('.carousel').carousel({
-    interval: 5000,
-    touch: true,
-})
-
-/* Checks if the url is one of the login/logout/password urls and hides the header */
-
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Fired!")
+    /* This snippet is required to render the Lucide Icons -> https://lucide.dev/guide/packages/lucide */
+    lucide.createIcons();
+
+    /* Checks if the url is one of the login/logout/password urls and hides the header */
     let loginUrl = window.location.href.includes('login');
     let logoutUrl = window.location.href.includes('logout');
     let signupUrl = window.location.href.includes('signup');
@@ -26,4 +18,58 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (resetUrl) {
         header.classList.add("hide");
     }
+
+    /* Custom settings for the bootstrap carousel -> https://getbootstrap.com/docs/4.6/components/carousel/ */
+    $('.carousel').carousel({
+        interval: 5000,
+        touch: true,
+    })
+
+    /* Looks for the mobile menu classes */
+    const mobileToggleOpen = document.getElementsByClassName("mobile-open")[0]
+    const mobileToggleClose = document.getElementsByClassName("mobile-close")[0]
+    const navBar = document.getElementsByClassName("mobile-menu")[0]
+    /* Looks for the search box classes */
+    const searchToggleOpen = document.getElementsByClassName("search-toggle")[0]
+    const searchToggleClose = document.getElementsByClassName("close-toggle")[0]
+    const searchBox = document.getElementsByClassName("search-box")[0]
+
+    /* Opens the search box and changes the icon*/
+    searchToggleOpen.addEventListener("click", () => {
+        searchBox.classList.remove("closed-search")
+        searchBox.classList.add("open-search")
+        searchToggleOpen.classList.add("d-none")
+        searchToggleClose.classList.remove("d-none")
+        
+    })
+
+    /* Closes the search box and changes the icon*/
+    searchToggleClose.addEventListener("click", () => {
+        searchBox.classList.remove("open-search")
+        searchBox.classList.add("closed-search")
+        searchToggleOpen.classList.remove("d-none")
+        searchToggleClose.classList.add("d-none")
+    })
+
+    /* Opens the mobile slide out menu and closes the search box*/
+    mobileToggleOpen.addEventListener("click", () => {
+        navBar.classList.remove("closed")
+        navBar.classList.add("open")
+        searchBox.classList.remove("open-search")
+        searchBox.classList.add("closed-search")
+    })
+    
+    //This does exactly the same as above, only with the close icon inside the sidebar and of course the other way around.
+    mobileToggleClose.addEventListener("click", () => {
+        navBar.classList.remove("open")
+        navBar.classList.add("closed")
+        searchBox.classList.remove("open-search")
+        searchBox.classList.add("closed-search")
+        searchToggleOpen.classList.remove("d-none")
+        searchToggleClose.classList.add("d-none")
+    })
+
 });
+
+
+
