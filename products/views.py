6382b087cd53c54b3604ae8ad4917c08cd2c357c
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-20 17:04:21 +0100

    Add home products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .models import Product
 
-# Create your views here.
+class ProductsHomeView(View):
+
+    def get_products(request):
+
+        products = Product.objects.all()
+        context = {
+            "products": products
+        }
+
+        return render(request, 'home/index.html', context)
+
