229f4321ae3ccec53ad399b76934f53eb3de793b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-20 17:05:06 +0100

    Add url

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from products.views import ProductsHomeView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', ProductsHomeView.as_view(), name='all_products'),
 ]
