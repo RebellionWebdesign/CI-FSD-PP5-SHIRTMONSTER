b90b6024d85f354fcd7b50630ddc0aff382e859b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-21 13:11:20 +0100

    Add model to view

--- products/views.py
+++ products/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Product
+from .models import Product, ProductCategory
 
 class ProductsHomeView(View):
 
     def get_products(request):
 
         products = Product.objects.all()
+        categories = ProductCategory.objects.all()
         context = {
-            "products": products
+            "products": products,
+            "categories": categories,
         }
 
         return render(request, 'home/index.html', context)
