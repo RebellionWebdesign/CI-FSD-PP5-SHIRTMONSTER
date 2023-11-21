8d5d7aaed9f55e8cb8505536069d627e795796fe
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-21 13:11:49 +0100

    Add model to view

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import View
-from products.models import Product
+from products.models import Product, ProductCategory
 
 class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
+        categories = ProductCategory.objects.all()
         context = {
-            "products": products
+            "products": products,
+            "categories": categories,
         }
     
         return render(request, 'home/index.html', context)
