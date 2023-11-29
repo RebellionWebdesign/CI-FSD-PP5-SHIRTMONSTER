6fa596d4dcd628717ea3a1404f38e22536918317
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-29 12:06:08 +0100

    Add category filter

--- shop/views.py
+++ shop/views.py
@@ -6,15 +6,23 @@ from products.models import Product, ProductCategory
 
 class ProductsShopView(View):
     """
-    A view which lets the user search for products on the shop page
+    A view which lets the user search for products on the shop page,
+    includes filtering and searching products
     """
     def get(self, request):
 
         products = Product.objects.all()
         categories = ProductCategory.objects.all()
         query = None
+        category = None
 
         if request.GET:
+
+            if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category_id__name__in=categories)
+                categories = ProductCategory.objects.filter(name__in=categories)
+
             if 'main-search' in request.GET:
                 query = request.GET['main-search']
                 if not query:
@@ -28,6 +36,7 @@ class ProductsShopView(View):
             "products": products,
             "categories": categories,
             "search_term": query,
+            "currents_categories": categories,
         }
 
         return render(request, 'shop/shop.html', context)
