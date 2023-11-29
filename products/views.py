c8e2d029b0b6fe2d5ba6e2a857b63f2ad0aa57d9
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-29 11:06:25 +0100

    Add docstring

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import View
 from .models import Product, ProductCategory
 
 class ProductsHomeView(View):
-
+    """
+    A view which gets all products from the database, so they can be
+    displayed on the homepage
+    """
     def get_products(request):
 
         products = Product.objects.all()
