20f11a8930b484a2150a09d86ac559c4cbfa1c5b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-30 14:21:13 +0100

    Add view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 def view_cart(request):
@@ -6,3 +6,26 @@ def view_cart(request):
     A view which lets shoppers view the shopping cart
     """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a specified product to the shopping cart """
+
+    quantity = 0
+
+    if quantity < 1:
+        quantity = 1
+    else:
+        quantity = int(request.POST.get('quantity'))
+
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
