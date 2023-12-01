db155ae1231c97918a30eb97ca7f6b83ab25d54c
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-01 15:00:11 +0100

    Add newline

--- users/models.py
+++ users/models.py
@@ -24,6 +24,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
+
 class UserAdress(models.Model):
     """
     This model stores the shipping data for a user.
@@ -42,4 +43,3 @@ class UserAdress(models.Model):
     
     def __str__(self):
         return self.user_id
-
