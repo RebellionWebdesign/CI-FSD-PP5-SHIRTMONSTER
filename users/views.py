75f2e14ec4a136dac8faa1ce8c9152d571582274
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-01 15:00:47 +0100

    Create user filtering view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from .models import UserAdress, UserProfile, User
 
-# Create your views here.
+class UserProfileView(LoginRequiredMixin, View):
+    template_name = 'users/user_profile.html'
+    context_object_name = 'user_profile'
+
+    def get(self, request, pk):
+
+        users = User.objects.filter(pk=pk)
+        context = {
+            "users": users,
+        }
+
+        return render(request, 'users/user_profile.html', context)
+    
+    def get_object(self):
+        return self.request.user
