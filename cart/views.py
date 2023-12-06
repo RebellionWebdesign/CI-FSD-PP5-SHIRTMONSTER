a5c456bd7f11668e530e0a356dcd922e4d0d3e9f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-06 17:14:47 +0100

    Add adjust cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views import View
 
 def view_cart(request):
@@ -23,3 +23,18 @@ def add_to_cart(request, item_id):
     
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart_quantity(request, item_id):
+    """ Remove a specified product from the shopping cart """
+
+    quantity = int(request.POST.get('quantity-counter'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
