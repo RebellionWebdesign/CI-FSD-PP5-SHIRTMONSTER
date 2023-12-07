8e73b6678d5f9e4bb1d4d7eeda1dce2e6736dcf1
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-07 11:11:41 +0100

    Add removal url

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='cart'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
-    path('remove/<item_id>', views.adjust_cart_quantity, name='adjust_cart_quantity'),
+    path('update/<item_id>', views.adjust_cart_quantity, name='update_cart_quantity'),
+    path('remove/<item_id>', views.remove_from_cart, name='remove_from_cart'),
 ]
