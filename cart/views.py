2e2fa0a4329b20017b88cab0e71e0d82c40b198d
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-07 12:39:42 +0100

    Add imports, messages

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 from django.views import View
+from products.models import Product
 
 def view_cart(request):
     """
@@ -11,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity-counter'))
 
     redirect_url = request.POST.get('redirect_url')
@@ -20,6 +23,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'{product.name} lives in your cart now!')
     
     request.session['cart'] = cart
     return redirect(redirect_url)
