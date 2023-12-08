ad3b03049eec7c9e84612c835889553577dbbff4
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-08 15:56:49 +0100

    Create wishlist app

---
+++ wishlist/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from products.models import Product
+from .models import User
+
+class WishList(models.Model):
+    """ this model stores the users wish list """
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist_user")
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlist_product")
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str("Wishlist")
