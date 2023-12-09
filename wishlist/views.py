0dbd5cbe07c08d39a6ac5c90bb22d928132c850f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-09 11:11:54 +0100

    Add wishlist view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from .models import WishList
 
-# Create your views here.
+class WishView(View):
+    """ This displays the users wishlist on the wishlist page """
+
+    def get(self, request):
+        wishes = WishList.objects.all()
+        context = {
+            "wishes": wishes,
+        }
+    
+        return render(request, 'wishlist/wishlist.html', context)
