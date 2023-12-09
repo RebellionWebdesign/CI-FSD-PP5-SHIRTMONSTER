0670ddb2356b698dd20df1fdd7080c2b0ae0b965
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-09 11:10:26 +0100

    Add url

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from .views import ProductDetailView
+from . import views
+
 
 
 urlpatterns = [
     path('show_product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('add_to_list/<int:pk>', views.add_to_wishlist, name='add_to_wishlist'),
 ]
