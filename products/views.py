af02d23cd2bc82032be9a2b10dd94802471239f4
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-09 11:10:40 +0100

    Add wishlist view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import View
 from .models import Product, ProductCategory
+from django.contrib import messages
+from products.models import Product
+from wishlist.models import WishList
 
 class ProductsHomeView(View):
     """
@@ -32,3 +35,17 @@ class ProductDetailView(View):
         }
 
         return render(request, 'products/product_detail.html', context)
+
+def add_to_wishlist(request, pk):
+    """ A view to let users add products to a wishlist """
+    
+    product = Product.objects.get(pk=pk)
+    user = request.user
+    wish = WishList()
+    wish.user_id = user
+    wish.product_id = product
+    wish.full_clean()
+    wish.save()
+    messages.success(request, 'Product added to wishlist!')
+    
+    return render(request, 'products/product_detail.html')
