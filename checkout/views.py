be5c7c7671610d645ff80f43c63042227fef1e2b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-11 13:05:49 +0100

    Change values

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,9 @@ from cart.contexts import cart_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'There is nothing in your bag!')
@@ -15,12 +18,19 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart["grand_total"]
     stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
-        'client_secret': settings.STRIPE_SECRET_KEY,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
+
