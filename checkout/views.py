e6432f4f72a57677127a596152b393da33e8db7b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-11 12:36:27 +0100

    Secure keys

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from users.models import UserAdress
 from .forms import OrderForm
+from cart.contexts import cart_contents
+import stripe
 
 def checkout(request):
     cart = request.session.get('cart', {})
@@ -9,10 +12,15 @@ def checkout(request):
         messages.error(request, 'There is nothing in your bag!')
         return redirect(reverse('shop'))
     
+    current_cart = cart_contents(request)
+    total = current_cart["grand_total"]
+    stripe_total = round(total*100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': settings.STRIPE_SECRET_KEY,
     }
 
     return render(request, template, context)
