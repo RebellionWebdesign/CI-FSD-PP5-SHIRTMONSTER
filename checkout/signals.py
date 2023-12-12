78b4c5a282ed406b4673ff281cb57348415b1336
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-12 12:15:57 +0100

    Fix signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -10,6 +10,7 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """ Updates the order_total value on item delete """
+    print("WORKS!")
     instance.order.update_total()
