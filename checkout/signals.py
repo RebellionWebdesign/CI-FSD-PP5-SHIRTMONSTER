57925629dfab2809854d743b6702d0f19dcec3e6
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-13 08:42:09 +0100

    Add print statement

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,11 +6,12 @@ from .models import OrderItem
 @receiver(post_save, sender=OrderItem)
 def update_on_save(sender, instance, created, **kwargs):
     """ Updates the order_total value on item update/create """
+    print("ON SAVE WORKS!")
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderItem)
 def update_on_delete(sender, instance, **kwargs):
     """ Updates the order_total value on item delete """
-    print("WORKS!")
+    print("ON DELETE WORKS!")
     instance.order.update_total()
