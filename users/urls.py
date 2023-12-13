8b3d70b48e8625eb49bd7e5a5c617e32c03cbb1f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-13 12:40:15 +0100

    Delete urls

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import UserProfileView
-
-
-urlpatterns = [
-    path('show_user/<int:pk>', UserProfileView.as_view(), name='user_profile'),
-]
