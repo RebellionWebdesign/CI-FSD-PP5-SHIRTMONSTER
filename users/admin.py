79365f007670246c6689d91f507916f3abf1c3b5
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-14 08:44:54 +0100

    Unregister from admin

--- users/admin.py
+++ users/admin.py
@@ -1,18 +1,1 @@
-from django.contrib import admin
-from .models import UserProfile, UserAdress
 
-
-class AdressAdmin(admin.ModelAdmin):
-    list_display = (
-        'user_id',
-        'adress_line_1',
-        'adress_line_2',
-        'city',
-        'zip_code',
-        'country',
-        'telephone',
-        'mobile_phone',
-    )
-
-admin.site.register(UserProfile)
-admin.site.register(UserAdress, AdressAdmin)
