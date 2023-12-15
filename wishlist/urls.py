0926f82c11ede32571f6760bcd8d0887b83e90c6
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-15 11:36:41 +0100

    Add wish removal url

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WishView
+from wishlist.models import WishList
+from .views import WishView, RemoveWish
 
 
 urlpatterns = [
     path('wishlist/', WishView.as_view(), name='user_wishlist'),
+    path('remove_from_list/<int:pk>', RemoveWish.as_view(), name='remove_from_wishlist'),
 ]
