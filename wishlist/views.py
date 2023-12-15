fb7661ed8d00af0988cf2c76cf9094d673ed39e2
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-15 11:36:56 +0100

    Add remove wish view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import View
+from products.models import Product
 from .models import WishList
+from django.contrib import messages
 
 class WishView(View):
     """ This displays the users wishlist on the wishlist page """
@@ -12,3 +14,16 @@ class WishView(View):
         }
     
         return render(request, 'wishlist/wishlist.html', context)
+
+
+class RemoveWish(View):
+    """ Deletes an item from the wishlist """
+
+    def post(self, request, pk):
+        wish_product = get_object_or_404(Product, pk=pk)
+        wish_item = get_object_or_404(WishList, product_id=wish_product)
+
+        wish_item.delete()
+        messages.success(request, 'Product removed from wishlist!')
+    
+        return redirect(reverse('user_wishlist'))
