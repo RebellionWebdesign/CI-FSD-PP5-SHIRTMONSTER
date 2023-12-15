e2f1d0922996da7af48ddd31022a1082270e3830
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-15 21:01:51 +0100

    Change text

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    This model stores the user profiles and their defaults.
+    This model stores the user profiles and their data.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, null=False, blank=False)
