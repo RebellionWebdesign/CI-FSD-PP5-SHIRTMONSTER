9c5884d666188fbef5b87972cca38e1b4eea8dd3
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-15 21:03:54 +0100

    Add form to context

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 from django.contrib.auth.models import User
+from . import forms
 
 class UserProfileView(LoginRequiredMixin, View):
     template_name = 'profiles/profiles.html'
@@ -10,8 +11,10 @@ class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, pk):
 
         users = User.objects.filter(pk=pk)
+        form = forms.UserProfileForm()
         context = {
             "users": users,
+            "form": form,
         }
 
         return render(request, 'profiles/profiles.html', context)
