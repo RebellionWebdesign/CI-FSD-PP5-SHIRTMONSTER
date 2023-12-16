dbb8dacd2e11fae6e52083bf20b206017c6cae47
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 12:55:26 +0100

    Delete printouts

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,12 +6,10 @@ from .models import OrderItem
 @receiver(post_save, sender=OrderItem)
 def update_on_save(sender, instance, created, **kwargs):
     """ Updates the order_total value on item update/create """
-    print("ON SAVE WORKS!")
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderItem)
 def update_on_delete(sender, instance, **kwargs):
     """ Updates the order_total value on item delete """
-    print("ON DELETE WORKS!")
     instance.order.update_total()
