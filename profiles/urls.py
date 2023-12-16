09282c19aba53b50a11ac160bf4b2927133aee51
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 12:57:21 +0100

    Add order history url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .views import UserProfileView
+from .views import UserProfileView, OrderHistory
 from django.urls import path
 
 urlpatterns = [
-    path('show_user/<int:pk>', UserProfileView.as_view() , name='profile')
+    path('show_user/<int:pk>', UserProfileView.as_view() , name='profile'),
+    path('order_history/<order_number>', OrderHistory.as_view() , name='order_history'),
 ]
