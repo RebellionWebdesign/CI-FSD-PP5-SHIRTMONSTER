4b5590951dc610dae303f6fe72ef52ba800b8b30
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 12:56:37 +0100

    Add user field and set update user

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,11 +1,27 @@
 from django import forms
 from .models import UserProfile
+from django.contrib.auth.models import User
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        fields = [
+            'phone',
+            'zip_code',
+            'city',
+            'adress_line_1',
+            'adress_line_2',
+            'country',
+            'user',
+        ]
+
+    user = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput())
+
+    def set_update_user(self, user):
+        self.fields['country'].widget.attrs['autofocus'] = True
+        self.fields['user'].initial = user
+
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
@@ -18,9 +34,11 @@ class UserProfileForm(forms.ModelForm):
             'city': 'Town or City',
             'adress_line_1': 'Street Address 1',
             'adress_line_2': 'Street Address 2',
+            'country': 'Country',
+            'user': 'Username',
         }
 
-        self.fields['country'].widget.attrs['autofocus'] = True
+        
         for field in self.fields:
             if field != 'country':
                 if self.fields[field].required:
