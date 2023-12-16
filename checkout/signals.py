8ef4db0e2fbcc7bef7cebba591a5a42b1c3991ca
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 15:19:49 +0100

    Add import

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import OrderItem
+from .models import Order, OrderItem
 
 
 @receiver(post_save, sender=OrderItem)
