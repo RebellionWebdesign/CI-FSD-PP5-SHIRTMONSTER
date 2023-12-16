fb17b158d4964ff109659aaf94c8d3a37f536087
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 15:22:05 +0100

    Remove unneeded import

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Order, OrderItem
+from .models import OrderItem
 
 
 @receiver(post_save, sender=OrderItem)
