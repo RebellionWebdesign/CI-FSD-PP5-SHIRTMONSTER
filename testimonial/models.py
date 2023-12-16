236587ef77ae0b33039c30b12be7ce1c7fb4747f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-16 19:26:07 +0100

    Add testimonial model

--- testimonial/models.py
+++ testimonial/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from checkout.models import Order
 
-# Create your models here.
+class Testimonial(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    order_id = models.OneToOneField(Order, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id
