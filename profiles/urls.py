f3138481b8db105b71f83bbafa26bfcec03c6917
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-18 13:43:20 +0100

    Delete url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .views import UserProfileView, OrderHistory
+from .views import UserProfileView
 from django.urls import path
 
 urlpatterns = [
     path('show_user/<int:pk>', UserProfileView.as_view() , name='profile'),
-    path('order_history/<order_number>', OrderHistory.as_view() , name='order_history'),
 ]
