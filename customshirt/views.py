47a16cf6f0d40345477d4b34f355e0415093a47e
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-18 07:46:56 +0100

    Update view for s3 image upload

--- customshirt/views.py
+++ customshirt/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.contrib import messages
 from .models import CustomShirt
 from .forms import CustomShirtForm
+import boto3
+import os
 
 class CustomShirtView(View):
     """ This view displays a contact form for custom shirt inquiries """
@@ -23,9 +25,26 @@ class CustomShirtView(View):
                 'phone': request.POST['phone'],
                 'inquiry': request.POST['inquiry'],
             }
-            inquiry_form = CustomShirtForm(form_data)
+            inquiry_form = CustomShirtForm(request.POST, request.FILES)
 
-            if inquiry_form.is_valid():
-                inquiry_form.save()
+        if inquiry_form.is_valid():
+            custom_shirt_instance = inquiry_form.save(commit=False)
+            client = boto3.client('s3', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'), aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
+            image = request.FILES['image']
+            image_location = f'custom_shirts/{image.name}'
+
+            try:
+                client.upload_fileobj(image, os.environ.get('AWS_BUCKET'), image_location)
+                custom_shirt_instance.image = image_location
+                custom_shirt_instance.save()
                 messages.success(request, 'Inquiry saved, we will contact you within 25 hours!')
-            return redirect(reverse(template))
+                return redirect(reverse(template))
+            except Exception as e:
+                messages.error(request, 'Error while uploading image, please contact us via eMail.')
+    
+            inquiry_form = CustomShirtForm(request.POST, request.FILES)
+            context =  {
+                'inquiry_form': inquiry_form
+            }
+
+            return render(request, 'customshirt/customshirt.html', context)
