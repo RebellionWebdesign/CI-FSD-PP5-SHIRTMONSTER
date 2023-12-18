100f6aafc140e395f88a2bac718a80b05196d275
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-18 09:27:52 +0100

    Format document

--- customshirt/forms.py
+++ customshirt/forms.py
@@ -26,7 +26,6 @@ class CustomShirtForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-
             placeholder = f'{placeholders[field]}'
             placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
