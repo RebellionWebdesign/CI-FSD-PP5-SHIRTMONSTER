433fb33e2e6f59bcfa65e26dade0db0da37919e3
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-18 09:28:15 +0100

    Add url field

--- customshirt/models.py
+++ customshirt/models.py
@@ -4,9 +4,10 @@ class CustomShirt(models.Model):
     """ This model saves inquiries for custom shirts """
     full_name = models.CharField(max_length=128)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone = models.CharField(max_length=20, null=False, blank=False)
-    inquiry = models.TextField()
-    image = models.ImageField(upload_to='custom_shirts', null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    inquiry = models.TextField(null=False, blank=False)
+    image = models.ImageField(upload_to='custom_shirts', null=False, blank=False)
+    image_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
