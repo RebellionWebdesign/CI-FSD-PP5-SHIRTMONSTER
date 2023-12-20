343844710ca782fbd8fd4774d8c2c348cb6466b8
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-20 20:37:07 +0100

    Add order detail url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .views import UserProfileView
+from .views import UserProfileView, OrderOverview
 from django.urls import path
 
 urlpatterns = [
     path('show_user/<int:pk>', UserProfileView.as_view() , name='profile'),
+    path('show_orders/<str:order_number>', OrderOverview.as_view() , name='order_detail'),
 ]
