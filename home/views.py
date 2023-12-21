2a80e24d3b8d0688007352dca88fa42ea1ca2df3
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-21 15:53:21 +0100

    Add testimonials to home view

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from products.models import Product, ProductCategory
+from testimonial.models import Testimonial
 
 class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
         categories = ProductCategory.objects.all()
+        testimonials = Testimonial.objects.all()
         context = {
             "products": products,
             "categories": categories,
+            "testimonials": testimonials,
         }
     
         return render(request, 'home/index.html', context)
