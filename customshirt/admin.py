a76ad8cd75eda48da427be5bd2cc37422253fdd3
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-21 14:13:33 +0100

    Register customshirt to admin

--- customshirt/admin.py
+++ customshirt/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CustomShirt
 
-# Register your models here.
+class CustomShirtAdmin(admin.ModelAdmin):
+    readonly_fields = ('full_name','email','phone','inquiry','image_tag','created_at',)
+    fields = ('image_url','inquiry','image_tag',)
+    list_display = (
+        'full_name',
+        'email',
+        'phone',
+        'inquiry',
+        'image',
+        'created_at',
+    )
+
+    ordering = ('-created_at',)
+
+admin.site.register(CustomShirt, CustomShirtAdmin)
