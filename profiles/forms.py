ec9c49db11a9538bd4de4c5bc7b3f12776919310
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-21 17:58:35 +0100

    Show labels

--- profiles/forms.py
+++ profiles/forms.py
@@ -46,4 +46,4 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].label = False
+            self.fields['phone'].label = "Phone"
