0abc056c983df871f88049371219a10789873fbc
Daniel Stauffer <info@rebellion-webdesign.com>
2023-12-21 14:41:48 +0100

    Register testimoials to admin

--- testimonial/admin.py
+++ testimonial/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+class TestimonialAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'created_at',
+        'modified_at',
+    ]
+    fields = (
+        'user_id',
+        'order_id',
+        'content',
+        'created_at',
+        'modified_at',
+    )
+    list_display = (
+        'user_id',
+        'order_id',
+        'created_at',
+        'modified_at',
+    )
+
+    ordering = ('-created_at',)
+
+admin.site.register(Testimonial, TestimonialAdmin)
