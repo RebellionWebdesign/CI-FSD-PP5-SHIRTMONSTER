2707bce0c31dfee971236e57ff881acac6531b64
Daniel Stauffer <info@rebellion-webdesign.com>
2024-01-23 17:35:02 +0100

    Fix issue, totals not saving, add tax

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,5 @@
 import uuid
+from decimal import Decimal
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
@@ -29,6 +30,8 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    tax = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
     original_cart = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(
         max_length=254, null=False, blank=False, default='')
@@ -39,18 +42,19 @@ class Order(models.Model):
 
     def update_total(self):
         """ Updates grand total and delivery cost for each new order item """
-        order_items = self.order_items.all()
-        self.order_total = order_items.aggregate(Sum('orderitem_total'))[
+        self.order_total = self.order_items.aggregate(Sum('orderitem_total'))[
             'orderitem_total__sum'] or 0
         print(self.order_total)
 
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.shipping_cost = self.order_total * \
                 settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            print(self.shipping_cost)
         else:
             self.shipping_cost = 0
-        self.grand_total = self.order_total + self.shipping_cost
-        self.save()
+        self.tax = self.order_total * Decimal(settings.TAX_PERCENTAGE)
+        self.grand_total = self.order_total + self.shipping_cost + self.tax
+        super().save()
 
     def save(self, *args, **kwargs):
         """ Override the default save method if there isnt an order number """
